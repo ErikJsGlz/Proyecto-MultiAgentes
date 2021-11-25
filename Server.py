351ed3d4800892c35677c28da6ea5bf2be4c8627
ErikJs219 <56207290+ErikJs219@users.noreply.github.com>
2021-11-24 18:46:29 -0600

    Se añadió grid al modelo.

---
+++ Server.py
@@ -0,0 +1,4 @@
+# Librerías para la transmisión de datos
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+import json
--- StreetModel.py
+++ StreetModel.py
@@ -6,19 +6,52 @@ from peaton import Peaton
 
 class StreetModel(ap.Model):
 
-    # Inicializamos los agentes
     def setup(self):
+        """ Inicializamos los agentes """
+        n_carros = self.p.carros
+        self.carros = ap.AgentList(self, n_carros, Carro)
+
+        n_semaforo_peatones = self.p.semaforo_peatones
+        self.semaforo_peatones = ap.AgentList(self, n_semaforo_peatones, SemaforoPeaton)
+
+        n_semaforo_carros = self.p.semaforo_carros
+        self.semaforo_carros = ap.AgentList(self, n_semaforo_carros, SemaforoCarro)
+
+        n_peatones = self.p.peatones
+        self.peatones = ap.AgentList(self, n_peatones, Peaton)
+
+
+        """ Creamos el grid del cruce """
+        size = self.p.size
+        self.cruce = ap.Grid(self, [size] * 2, track_empty = True)
+
+        # Agregamos a los agentes
+        """ 
+        Random: para asignar posiciones aleatorias 
+        empty: ?
         
-        self.carros = ap.AgentList(self, self.p.carros, Carro)
-        self.semaforo_peatones = ap.AgentList(self, self.p.semaforo_peatones, SemaforoPeaton)
-        self.semaforo_carros = ap.AgentList(self, self.p.semaforo_carros, SemaforoCarro)
-        self.peatones = ap.AgentList(self, self.p.peatones, Peaton)
+        
+        """
+        self.cruce.add_agents(self.carros, [(2, 3), (4, 6)])
+        # self.carros[0].x = 2
+
+        for i in range(2):
+            if (i == 0):
+                self.carros[i].x = 2
+                self.carros[i].y = 3
+
+            elif (i == 1):
+                self.carros[i].x = 4
+                self.carros[i].y = 6
+
+        # carros.x
+        # carros[0].x
         
         #print("Hay un total de: " + str(self.p.carros) + " carros")
         # for car in self.carros:
-        #     print(car.x)
+        #     print(car.x)}
 
-    def step(self):
+    def step(self): 
         #self.cars.agent_method()
         pass
         # self.carros.move_left(2)
@@ -41,11 +74,12 @@ class StreetModel(ap.Model):
 
         
 parameters = {
-    'carros': 10,
-    'semaforo_peatones': 2,
-    'semaforo_carros': 2,
-    'peatones': 20,
-    'steps': 10,
+    'carros': 2, # número de agentes Carro
+    'semaforo_peatones': 2, # número de agentes Semáforos para peatones
+    'semaforo_carros': 2, # número de agentes Semáforos para carros
+    'peatones': 20, # número para agentes Peatones
+    'size': 100, # Largo y alto del grid
+    'steps': 10, # iteraciones
     'seed': 40,
 }
 
