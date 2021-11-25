7586610964b2d50f80d00d217ad2f9d7a60d2108
ErikJs219 <56207290+ErikJs219@users.noreply.github.com>
2021-11-24 22:20:58 -0600

    Se añadió el export de la posición en JSON

--- StreetModel.py
+++ StreetModel.py
@@ -4,6 +4,11 @@ from semaforo_p import SemaforoPeaton
 from carro import Carro
 from peaton import Peaton
 
+# Importamos JSON
+import json
+# Posiciones en formato JSON
+posJSON = []
+
 class StreetModel(ap.Model):
 
     def setup(self):
@@ -26,27 +31,22 @@ class StreetModel(ap.Model):
         self.cruce = ap.Grid(self, [size] * 2, track_empty = True)
 
         """ Agregamos a los agentes de tipo Carro """
-        # Los ponemos en 
-        x = [2, 4]
-        y = [3, 6]
+        # Definimos la posición de los agentes dentro del Grid
         self.cruce.add_agents(self.carros, [
-            (2, 3), 
-            (4, 6)
+            (3, -16), 
+            (3, -23),
+            (-30, -3),
+            (-37, -3)
         ])
 
-        for i in range(2):
-            if (i == 0):
-                self.carros[i].x = 2
-                self.carros[i].y = 3
-
-            elif (i == 1):
-                self.carros[i].x = 4
-                self.carros[i].y = 6
-
-        
-        #print("Hay un total de: " + str(self.p.carros) + " carros")
-        # for car in self.carros:
-        #     print(car.x)}
+        # Definimos la posisición de los carros para mandarlo a Unity
+        # Para avanzar[0 - 1] -> z++, [2, 3] -> x++
+        x_carro = [3, 3, -30, -37]
+        z_carro = [-16, -23, -3, -3]
+        for i in range(4):
+            self.carros[i].x = x_carro[i]
+            self.carros[i].y = 0
+            self.carros[i].z = z_carro[i]
 
     def step(self): 
         #self.cars.agent_method()
@@ -57,29 +57,44 @@ class StreetModel(ap.Model):
         
         # Meter agente semáforo #
 
-        print("\n" + str(self.carros.x))
-        count = 0
-        for carro in self.carros:
-            count += 1
-            if (count % 2 == 0):
-                carro.move_left(2)
-            else:
-                carro.move_right(2)
+        print("\n")
+        for i in range(4):
+            print("Carro " + str(i + 1) + " - Posición: (" + str(self.carros[i].x) + ", " + str(self.carros[i].y) + ", " + str(self.carros[i].z) + ")" )
+        
+        # Hacemos que avancen los carros
+        for i in range(4):
+            if (i == 0 or i == 1):
+                self.carros[i].move_up(1)
+            elif (i == 2 or i == 3):
+                self.carros[i].move_right(1)
+
+        for i in range(4):
+            pos = {
+                "x" : self.carros[i].x,
+                "y" : self.carros[i].y,
+                "z" : self.carros[i].z
+            }
+            posJSON.append(pos)
         
     def end(self):
         self.report('ended', 1) 
 
         
 parameters = {
-    'carros': 2, # número de agentes Carro
+    'carros': 4, # número de agentes Carro
     'semaforo_peatones': 2, # número de agentes Semáforos para peatones
     'semaforo_carros': 2, # número de agentes Semáforos para carros
     'peatones': 20, # número para agentes Peatones
     'size': 100, # Largo y alto del grid
-    'steps': 10, # iteraciones
+    'steps': 100, # iteraciones
     'seed': 40,
 }
 
 model = StreetModel(parameters)
 results = model.run()
 print(results)
+
+# Guardamos el JSON como archivo
+json_object = json.dumps(posJSON, indent = 4)
+with open("positions.json", "w") as outfile:
+    outfile.write(json_object)
