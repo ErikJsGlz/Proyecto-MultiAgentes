662d250402c1ffdff4ee8367ea782aadf616368b
ErikJs219 <56207290+ErikJs219@users.noreply.github.com>
2021-11-27 23:24:08 -0600

    Se añadieron los semáforos.

--- StreetModel.py
+++ StreetModel.py
@@ -4,23 +4,35 @@ from semaforo_p import SemaforoPeaton
 from carro import Carro
 from peaton import Peaton
 
+# Para el tiempo
+import time
+t = 0
+elapsed_time = 0
+
 # Importamos JSON
 import json
 # Posiciones en formato JSON
 posJSON = []
+currentPos = {}
 
 class StreetModel(ap.Model):
 
     def setup(self):
+        # Iniciamos el contador de segundos
+        global t
+        t = time.time()
+
+        self.semaforoActivo = 0
+
         """ Inicializamos los agentes """
         n_carros = self.p.carros
         self.carros = ap.AgentList(self, n_carros, Carro)
 
-        n_semaforo_peatones = self.p.semaforo_peatones
-        self.semaforo_peatones = ap.AgentList(self, n_semaforo_peatones, SemaforoPeaton)
-
         n_semaforo_carros = self.p.semaforo_carros
         self.semaforo_carros = ap.AgentList(self, n_semaforo_carros, SemaforoCarro)
+
+        n_semaforo_peatones = self.p.semaforo_peatones
+        self.semaforo_peatones = ap.AgentList(self, n_semaforo_peatones, SemaforoPeaton)
 
         n_peatones = self.p.peatones
         self.peatones = ap.AgentList(self, n_peatones, Peaton)
@@ -38,7 +50,6 @@ class StreetModel(ap.Model):
             (-30, -3),
             (-37, -3)
         ])
-
         # Definimos la posisición de los carros para mandarlo a Unity
         # Para avanzar[0 - 1] -> z++, [2, 3] -> x++
         x_carro = [3, 3, -30, -37]
@@ -48,18 +59,57 @@ class StreetModel(ap.Model):
             self.carros[i].y = 0
             self.carros[i].z = z_carro[i]
 
-    def step(self): 
-        #self.cars.agent_method()
-        pass
-        # self.carros.move_left(2)
 
-    def update(self):
-        
-        # Meter agente semáforo #
+        """ Agregamos a los agentes de tipo Semaforos """
+        # Lo asignamos en posiciones específicas en el grid
+        self.cruce.add_agents(self.semaforo_carros, [
+            (-7, 7),
+            (7, -7)
+        ])
+        self.cruce.add_agents(self.semaforo_peatones, [
+            (-6, -7),
+            (6, 7)
+        ])
+        # Definimos la posición de los semaforo para mandarlo a Unity
+        x_semaforo_carro = [-7, 7]
+        z_semaforo_carro = [7, -7]
+        x_semaforo_peatones = [-6, 6]
+        z_semaforo_peatones = [-7, 7]
+
+        for i in range(n_semaforo_carros):
+            self.semaforo_carros[i].x = x_semaforo_carro[i]
+            self.semaforo_carros[i].y = 0
+            self.semaforo_carros[i].z = z_semaforo_carro[i]
+
+            pos = {
+                "semaforo_carro" + str(i) + "_x" : x_semaforo_carro[i],
+                "semaforo_carro" + str(i) + "_y" : z_semaforo_carro[i]
+            }
+            currentPos.update(pos)
+
+        for i in range(n_semaforo_peatones):
+            self.semaforo_peatones[i].x = x_semaforo_peatones[i]
+            self.semaforo_peatones[i].y = 0
+            self.semaforo_peatones[i].z = z_semaforo_peatones[i]
+
+            pos = {
+                "semaforo_peatones" + str(i) + "_x" : x_semaforo_peatones[i],
+                "semaforo_peatones" + str(i) + "_y" : z_semaforo_peatones[i]
+            }
+            currentPos.update(pos)
+
+        posJSON.append(currentPos)
+
+        ### AÑADIR PEATONES ###
+
+    def step(self): 
+
+        # Reiniciamos el currentPos para iniciar las posiciones de los carros
+        currentPos = {}
 
         print("\n")
-        for i in range(4):
-            print("Carro " + str(i + 1) + " - Posición: (" + str(self.carros[i].x) + ", " + str(self.carros[i].y) + ", " + str(self.carros[i].z) + ")" )
+        # for i in range(4):
+        #     print("Carro " + str(i + 1) + " - Posición: (" + str(self.carros[i].x) + ", " + str(self.carros[i].y) + ", " + str(self.carros[i].z) + ")" )
         
         # Hacemos que avancen los carros
         for i in range(4):
@@ -70,11 +120,37 @@ class StreetModel(ap.Model):
 
         for i in range(4):
             pos = {
-                "x" : self.carros[i].x,
-                "y" : self.carros[i].y,
-                "z" : self.carros[i].z
+                "carro" + str(i) + "_x" : self.carros[i].x,
+                "carro" + str(i) + "_y" : self.carros[i].y,
+                "carro" + str(i) + "_z" : self.carros[i].z
             }
-            posJSON.append(pos)
+            currentPos.update(pos)
+
+        #  #
+        if (self.semaforoActivo == 0):
+            self.semaforo_carros[0].changeStatus()
+            self.semaforoActivo = 1
+            print("Semaforo 1")
+        else:
+            self.semaforo_carros[1].changeStatus()
+            self.semaforoActivo = 0
+            print("Semaforo 2")
+        
+        
+
+        # Vemos cuanto tiempo pasó #
+        global elapsed_time
+        elapsed_time = time.time() - t
+        print(elapsed_time)
+
+
+        posJSON.append(currentPos)
+
+        ### ¿Falta actualizarlo en el grid? ###
+        ### AÑADIR ALGUNA FUNCIONALIDAD A LOS PEATONES ###
+        ### AÑADIR FUNCIONALIDAD DE LOS SEMÁFOROS ###
+        
+        
         
     def end(self):
         self.report('ended', 1) 
